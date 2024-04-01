2d8732a32de13888b5ef9132ef13a4001deeaa6d
Gunasekaran-Annamalai <gunasekaran021098@gmail.com>
2024-04-01 13:43:16 +0530

    first commit

---
+++ CustomMiddlewareAPI/CustomMiddlewareAPI/Controllers/CustuomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMiddlewareAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustuomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Middleware works");
+        }
+    }
+}
---
+++ CustomMiddlewareAPI/CustomMiddlewareAPI/Middleware/CustomMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddlewareAPI.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public CustomMiddleware(RequestDelegate next, ILoggerFactory logger)
+        {
+            _next = next;
+            _logger = logger.CreateLogger("Custom Middleware");
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("Custom Middleware Initiated");
+            await _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
